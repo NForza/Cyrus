027daca91056998d1f356d9ba98d2db0ed0c7942
Thomas Huijer <thomas@nforza.nl>
2025-01-27 18:28:17 +0100

    Generate QueryReturnType in Typescript (#72)

---
+++ src/NForza.Cyrus.TypescriptGenerate/Model/HubQueryReturnType.cs
@@ -0,0 +1,8 @@
+﻿namespace NForza.Cyrus.TypescriptGenerate.Model;
+
+public class HubQueryReturnType
+{
+    public string Name { get; set; } = string.Empty;
+    public bool IsCollection { get; set; }
+    public bool IsNullable { get; set; }
+}
