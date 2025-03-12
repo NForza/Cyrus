ae1979d9815182eec5d98e10bcf5a497cc1dc272
Thomas Huijer <thomas@nforza.nl>
2025-03-12 14:50:37 +0100

    Fix tests

--- examples/DemoApp/DemoApp.WebApi.Tests/CustomerTests.cs
+++ examples/DemoApp/DemoApp.WebApi.Tests/CustomerTests.cs
@@ -22,6 +22,15 @@ public class CustomerTests(ITestOutputHelper testOutput)
     }
 
     [Theory]
+    [InlineData("/customers/0c81023e-8dbb-4cab-95a9-99f6057f81de")]
+    public async Task Getting_Customer_By_ID_Should_Succeed(string url)
+    {
+        var response = await client.GetAsync(url);
+        response.Should().NotBeNull();
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Theory]
     [InlineData("/customers")]
     public async Task Posting_Add_Customer_Command_Should_Succeed(string url)
     {
@@ -44,42 +53,17 @@ public class CustomerTests(ITestOutputHelper testOutput)
     }
 
     [Theory]
-    [InlineData("/customers")]
+    [InlineData("/customers/{Id}")]
     public async Task Putting_Update_Customer_Command_Should_Succeed(string url)
     {
-        var command = new UpdateCustomerCommand(new CustomerId(), new Name("Thomas"), new Address(new Street("Main Street"), new StreetNumber(12)));
-        var response = await client.PutAsJsonAsync(url, command);
+        var customerId = new CustomerId();
+        var command = new UpdateCustomerCommand(customerId, new Name("Thomas"), new Address(new Street("Main Street"), new StreetNumber(12)));
+        var response = await client.PutAsJsonAsync(url.Replace("{Id}", customerId.ToString()), command);
         response.Should().NotBeNull();
         var content = await response.Content.ReadAsStringAsync();
 
         response.StatusCode.Should().Be(HttpStatusCode.Accepted);
         response.Headers.Location?.ToString().Should().NotBeNullOrEmpty();
         response.Headers.Location!.ToString().Should().StartWith("/customers/");
-    }
-
-    [Theory]
-    [InlineData("/customers")]
-    public async Task Deleting_Customer_Command_Without_ApiKey_Header_Should_Fail(string url)
-    {
-        var command = new DeleteCustomerCommand(new CustomerId());
-        var response = await client.DeleteAsync(url + $"/{command.Id}");
-        response.Should().NotBeNull();
-        var content = await response.Content.ReadAsStringAsync();
-
-        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        content.Should().Contain("ApiKey not present");
-    }
-
-    [Theory]
-    [InlineData("/customers")]
-    public async Task Deleting_Customer_Command_With_ApiKey_Header_Should_Succeed(string url)
-    {
-        var command = new DeleteCustomerCommand(new CustomerId());
-        client.DefaultRequestHeaders.Add("ApiKey", "1234");
-        var response = await client.DeleteAsync(url + $"/{command.Id}");
-        response.Should().NotBeNull();
-        var content = await response.Content.ReadAsStringAsync();
-
-        response.StatusCode.Should().Be(HttpStatusCode.Accepted);
     }
 }
