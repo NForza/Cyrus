0ac5fee7311e232d5710742c363f8e910da74de7
Thomas Huijer <thomas@nforza.nl>
2025-04-16 17:21:48 +0200

    Add support for analyzers when running generator tests

---
+++ NForza.Cyrus.Generators.Tests/CyrusGeneratorTestBuilder.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using NForza.Cyrus.Generators.Analyzers;
+
+namespace NForza.Cyrus.Generators.Tests
+{
+    internal class CyrusGeneratorTestBuilder
+    {
+        private string? source;
+
+        public (ImmutableArray<Diagnostic> compilerOutput, ImmutableArray<Diagnostic> analyzerOutput, string generatedSource) Run()
+        {
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                throw new InvalidOperationException("Source code is not set.");
+            }
+
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+            var referencedAssemblies = new string[]
+            {
+                typeof(IServiceCollection).Assembly.Location
+            };
+
+            var trustedAssemblies = ((string?)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES"))!
+                .Split(Path.PathSeparator)
+                .Concat(referencedAssemblies)
+                .Distinct()
+                .Select(path => MetadataReference.CreateFromFile(path))
+                .ToList();
+
+            var compilation = CSharpCompilation.Create(
+                "TestAssembly",
+                [syntaxTree],
+                trustedAssemblies,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+            );
+
+            IIncrementalGenerator generator = new CyrusGenerator();
+
+            GeneratorDriver driver = CSharpGeneratorDriver.Create([generator]);
+
+            driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
+            var compilationWithAnalyzers = outputCompilation.WithAnalyzers([new CyrusAnalyzer()]);
+
+            var compileDiagnostics = outputCompilation.GetDiagnostics();
+            var analyzerDiagnostics = compilationWithAnalyzers.GetAnalyzerDiagnosticsAsync().Result;
+
+            var generatedCode = outputCompilation.SyntaxTrees
+                .Skip(1)
+                .Select(tree => tree.ToString())
+                .FirstOrDefault();
+
+            return (compileDiagnostics, analyzerDiagnostics, generatedCode ?? string.Empty);
+        }
+
+        internal CyrusGeneratorTestBuilder WithSource(string source)
+        {
+            this.source = source;
+            return this;
+        }
+    }
+}
--- NForza.Cyrus.Generators.Tests/GeneratorTest.cs
+++ NForza.Cyrus.Generators.Tests/GeneratorTest.cs
@@ -1,16 +1,14 @@
 ﻿using FluentAssertions;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace NForza.Cyrus.Generators.Tests
+namespace NForza.Cyrus.Generators.Tests;
+
+public class GeneratorTest
 {
-    public class GeneratorTest
+    [Fact]
+    public void Generating_GuidId_Should_Compile_And_Generate_Partial_Record_Struct()
     {
-        [Fact]
-        public void Generating_GuidId_Should_Compile_And_Generate_Partial_Record_Struct()
-        {
-            var source = @"
+        var source = @"
                 using NForza.Cyrus.Abstractions;
 
                 namespace Demo;
@@ -19,44 +17,34 @@ namespace NForza.Cyrus.Generators.Tests
                 public partial record struct CustomerId; 
             ";
 
-            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        (var compilerOutput, var analyzerOutput, var generatedCode) = new CyrusGeneratorTestBuilder()
+            .WithSource(source)
+            .Run();
 
-            var referencedAssemblies = new string[]
-            {
-                typeof(IServiceCollection).Assembly.Location
-            };
+        compilerOutput.Where(d => d.Severity == DiagnosticSeverity.Error).Should().HaveCount(0);
+        analyzerOutput.Should().HaveCount(0);
 
-            var trustedAssemblies = ((string?)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES"))!
-                .Split(Path.PathSeparator)
-                .Concat(referencedAssemblies)
-                .Distinct()
-                .Select(path => MetadataReference.CreateFromFile(path))
-                .ToList();
+        Assert.NotNull(generatedCode);
+        generatedCode.Should().Contain("partial record struct CustomerId(Guid Value): IGuidId, IComparable<CustomerId>, IComparable");
+    }
 
-            var compilation = CSharpCompilation.Create(
-                "TestAssembly",
-                [syntaxTree],
-                trustedAssemblies,
-                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
-            );
+    [Fact]
+    public void Generating_GuidId_For_Struct_Which_Is_Not_A_Record_Should_Not_Compile()
+    {
+        var source = @"
+                using NForza.Cyrus.Abstractions;
 
-            IIncrementalGenerator generator = new CyrusGenerator(); 
+                namespace Demo;
+            
+                [GuidId]
+                public partial struct CustomerId; 
+            ";
 
-            GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+        (var compilerOutput, var analyzerOutput, var generatedCode) = new CyrusGeneratorTestBuilder()
+            .WithSource(source)
+            .Run();
 
-            driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
-
-            var compileDiagnostics = outputCompilation.GetDiagnostics();
-            var errors = compileDiagnostics.Where(d => d.Severity == DiagnosticSeverity.Error);
-            errors.Should().HaveCount(0);
-
-            var generatedCode = outputCompilation.SyntaxTrees
-                .Skip(1) 
-                .Select(tree => tree.ToString())
-                .FirstOrDefault();
-
-            Assert.NotNull(generatedCode);
-            generatedCode.Should().Contain("partial record struct CustomerId(Guid Value): IGuidId, IComparable<CustomerId>, IComparable");
-        }
+        analyzerOutput.Where(d => d.Severity == DiagnosticSeverity.Error).Should().HaveCount(1);
+        analyzerOutput.First().Descriptor.Should().Be(DiagnosticDescriptors.TypedIdMustBeARecordStruct);
     }
 }
