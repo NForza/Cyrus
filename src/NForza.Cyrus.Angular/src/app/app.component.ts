392c58ba288956260f03779ec0d7618bb1dcccca
Thomas Huijer <thomas@nforza.nl>
2025-04-22 18:05:32 +0200

    generate camelcased properties

--- src/NForza.Cyrus.Angular/src/app/app.component.ts
+++ src/NForza.Cyrus.Angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { CustomerHubService } from './signalr.service';
+import { CustomerHubService } from './generated/CustomerHub';
+import { CustomerType } from './generated/CustomerType';
+import { CustomerAddedEvent } from './generated/CustomerAddedEvent';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +11,30 @@ import { CustomerHubService } from './signalr.service';
 export class AppComponent {
   title = 'CyrusApp';
   
-  messages: { id: string, name: string, address: string }[] = [];
+  public messages: CustomerAddedEvent[] = [];
 
   constructor(private signalRService: CustomerHubService) {
     signalRService.startConnection();
+    signalRService.onCustomerAdded.subscribe(e => this.addToList(e));
+  }
+
+  private addToList(c: CustomerAddedEvent): void {
+    this.messages = [
+      ...this.messages,                                            
+      c
+    ];
   }
 
   ngOnInit(): void {
-
+    this.signalRService.startConnection();
   }
 
   sendMessage(): void {
-    this.signalRService.addCustomer({ "name": "name1", "address": "address1"});
+    this.signalRService.addCustomerCommand(
+      { 
+        customerType: CustomerType.Company,
+        id: crypto.randomUUID(),
+        name: "name1", 
+        address: { street: "Street", streetNumber: 1 }});
   }
 }
