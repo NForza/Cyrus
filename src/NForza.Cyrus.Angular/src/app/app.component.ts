05eeff21821e3a865926b41f2b8fc9d93b308bc7
Thomas Huijer <thomas@nforza.nl>
2025-04-22 16:47:51 +0200

    Update Angular App

--- src/NForza.Cyrus.Angular/src/app/app.component.ts
+++ src/NForza.Cyrus.Angular/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { CustomerHubService } from './generated/CustomerHub';
-import { CustomerType } from './generated/CustomerType';
-import { CustomerAddedEvent } from './generated/CustomerAddedEvent';
+import { CustomerHubService } from './signalr.service';
 
 @Component({
   selector: 'app-root',
@@ -11,30 +9,17 @@ import { CustomerAddedEvent } from './generated/CustomerAddedEvent';
 export class AppComponent {
   title = 'CyrusApp';
   
-  public messages: CustomerAddedEvent[] = [];
+  messages: { id: string, name: string, address: string }[] = [];
 
   constructor(private signalRService: CustomerHubService) {
     signalRService.startConnection();
-    signalRService.onCustomerAdded.subscribe(e => this.addToList(e));
-  }
-
-  private addToList(c: CustomerAddedEvent): void {
-    this.messages = [
-      ...this.messages,                                            
-      c
-    ];
   }
 
   ngOnInit(): void {
-    this.signalRService.startConnection();
+
   }
 
   sendMessage(): void {
-    this.signalRService.addCustomerCommand(
-      { 
-        customerType: CustomerType.Company,
-        id: crypto.randomUUID(),
-        name: "name1", 
-        address: { street: "Street", streetNumber: 1 }});
+    this.signalRService.addCustomer({ "name": "name1", "address": "address1"});
   }
 }
