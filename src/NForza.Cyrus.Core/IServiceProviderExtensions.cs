92d86aec5ad309425457456480ca0ad5708bd7e2
Thomas Huijer <thomas@nforza.nl>
2025-04-25 16:55:00 +0200

    Generate model file after a successful build (#169)

---
+++ src/NForza.Cyrus.Core/IServiceProviderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using NForza.Cyrus.Model;
+
+namespace NForza.Cyrus;
+
+public static class IServiceProviderExtensions
+{
+    public static bool OutputCyrusModelOnly(this IServiceProvider serviceProvider, string[] args)
+    {
+        if (args.Length == 0 || args[0] != "--generateModel")
+        {
+            return false;
+        }
+        var generator = CyrusModel.Aggregate(serviceProvider);
+        var fileName = args.Length > 1 ? args[1] : "cyrus.json";
+        File.WriteAllText(fileName, generator.AsJson());
+        return true;
+    }
+}
