8be18ad4591de3714e29d1d4c35f0a8c79a4d3cb
Thomas Huijer <thomas@nforza.nl>
2025-05-27 08:59:23 +0200

    Registration of persistence (WIP)

--- src/NForza.Cyrus.Generators/EntityFramework/EntityFrameworkPersistenceGenerator.cs
+++ src/NForza.Cyrus.Generators/EntityFramework/EntityFrameworkPersistenceGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
@@ -15,15 +16,27 @@ public class EntityFrameworkPersistenceGenerator : CyrusGeneratorBase
     public override void GenerateSource(SourceProductionContext spc, CyrusGenerationContext cyrusProvider)
     {
         var config = cyrusProvider.GenerationConfig;
-        if (!string.IsNullOrEmpty(config.PersistenceContextType))
+        if (!string.IsNullOrEmpty(config.PersistenceContextType) && cyrusProvider.AggregateRoots.Any())
         {
-                var sourceText = GenerateEntityFrameworkPersistenceInitializer(config.PersistenceContextType, cyrusProvider.LiquidEngine);
-                spc.AddSource($"EntityFrameworkPersistenceInitializer.g.cs", SourceText.From(sourceText, Encoding.UTF8));
+            var sourceText = GenerateEntityFrameworkPersistenceInitializer(config.PersistenceContextType, cyrusProvider);
+            spc.AddSource($"EntityFrameworkPersistenceInitializer.g.cs", SourceText.From(sourceText, Encoding.UTF8));
         }
     }
 
-    private string GenerateEntityFrameworkPersistenceInitializer(string entityFrameworkPersistenceType, LiquidEngine liquidEngine)
+    private string GenerateEntityFrameworkPersistenceInitializer(string entityFrameworkPersistenceType, CyrusGenerationContext cyrusGenerationContext)
     {
-        return "";
+        //var persistenceRegistrations = string.Join(Environment.NewLine, cyrusGenerationContext.AggregateRoots.Select(v =>
+        //    $"services.AddTransient<IAggregateRootPersistence, EntityFrameworkPersistence<{v.ToFullName()}>();"));
+        var persistenceRegistrations = "";
+        var ctx = new
+        {
+            Usings = new string[] { },
+            Namespace = "Persistence",
+            Name = "PersistenceRegistration",
+            Initializer = persistenceRegistrations
+        };
+
+        var fileContents = cyrusGenerationContext.LiquidEngine.Render(ctx, "CyrusInitializer");
+        return fileContents;
     }
 }
