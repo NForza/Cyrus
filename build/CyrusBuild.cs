4ca7fd9533f116082e017e1104accf7beebf6369
Thomas Huijer <thomas@nforza.nl>
2025-06-01 14:52:35 +0200

    Add examples to Nuke build

--- build/CyrusBuild.cs
+++ build/CyrusBuild.cs
@@ -2,13 +2,15 @@ using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.Tools.DotNet;
 
-
 class CyrusBuild : NukeBuild
 {
     public static int Main() => Execute<CyrusBuild>(x => x.Compile);
 
     private static string cyrusSolutionFileName = "NForza.Cyrus.sln";
     private static AbsolutePath cyrusSolutionPath = RootDirectory / cyrusSolutionFileName;
+    private static AbsolutePath examplesPath = RootDirectory / "examples";
+    private static AbsolutePath massTransitExamplePath = examplesPath / "MassTransit" / "MassTransit.sln";
+    private static AbsolutePath signalRExamplePath = examplesPath / "SignalR" / "CyrusSignalR" / "CyrusSignalR.sln";
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -36,6 +38,14 @@ class CyrusBuild : NukeBuild
                 .SetProjectFile(cyrusSolutionPath)
                 .SetConfiguration(Configuration)
                 .EnableNoRestore());
+
+            DotNetTasks.DotNetBuild(s => s
+                .SetProjectFile(massTransitExamplePath)
+                .SetConfiguration(Configuration));
+
+            DotNetTasks.DotNetBuild(s => s
+                .SetProjectFile(signalRExamplePath)
+                .SetConfiguration(Configuration));
         });
 
     Target Test => _ => _
