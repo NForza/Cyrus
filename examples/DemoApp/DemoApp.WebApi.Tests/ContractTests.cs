e20e08ed4dacd9faf13567ee01514587bd62089f
Thomas Huijer <thomas@nforza.nl>
2025-06-28 09:15:09 +0200

    Support regex validation for string value types

---
+++ examples/DemoApp/DemoApp.WebApi.Tests/ContractTests.cs
@@ -0,0 +1,29 @@
+﻿using DemoApp.Contracts;
+using FluentAssertions;
+
+namespace DemoApp.WebApi.Tests;
+
+public class ContractTests
+{
+    [Fact]
+    public void Name_That_Has_No_Invalid_Characters_Should_Be_Valid()
+    {
+        // Arrange
+        var name = new Name("John Doe");
+        // Act 
+        var isValid = name.IsValid();
+        // Assert
+        isValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Name_That_Has_Invalid_Characters_Should_Not_Be_Valid()
+    {
+        // Arrange
+        var name = new Name("X9");
+        // Act 
+        var isValid = name.IsValid();
+        // Assert
+        isValid.Should().BeFalse();
+    }
+}
