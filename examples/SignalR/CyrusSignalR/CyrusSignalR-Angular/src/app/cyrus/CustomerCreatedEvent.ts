f7b29b797f9d3599fa6fd1d0f26069b771906ecc
Thomas Huijer <thomas@nforza.nl>
2025-10-31 08:31:46 +0100

    Update packages in examples

--- examples/SignalR/CyrusSignalR/CyrusSignalR-Angular/src/app/cyrus/CustomerCreatedEvent.ts
+++ examples/SignalR/CyrusSignalR/CyrusSignalR-Angular/src/app/cyrus/CustomerCreatedEvent.ts
@@ -2,7 +2,7 @@ import { CustomerId } from './CustomerId';
 import { CustomerId } from './CustomerId';
 import { Name } from './Name';
 import { Name } from './Name';
-export interface AddCustomerCommand {
+export interface CustomerCreatedEvent {
     customerId: CustomerId;
     name: Name;
 }
--- examples/SignalR/CyrusSignalR/CyrusSignalR-Angular/src/app/cyrus/CustomerId.ts
+++ examples/SignalR/CyrusSignalR/CyrusSignalR-Angular/src/app/cyrus/CustomerId.ts
@@ -1,1 +1,2 @@
-export type CustomerId = string;
+export interface CustomerId {
+}
