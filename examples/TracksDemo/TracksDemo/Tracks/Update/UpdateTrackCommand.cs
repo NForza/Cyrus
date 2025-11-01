92880d41205ecffecbc30cf84ef374057c476008
Thomas Huijer <thomas@nforza.nl>
2025-11-01 10:36:53 +0100

    Layout cleanup

--- examples/TracksDemo/TracksDemo/Tracks/Update/UpdateTrackCommand.cs
+++ examples/TracksDemo/TracksDemo/Tracks/Update/UpdateTrackCommand.cs
@@ -4,4 +4,4 @@ using NForza.Cyrus.Aggregates;
 namespace TracksDemo.Tracks.Update;
 
 [Command]
-public record UpdateTrackCommand([property:AggregateRootId] TrackId TrackId, Title Title, Artist Artist, FileName FileName, AudioFormat AudioFormat);
+public record UpdateTrackCommand([property: AggregateRootId] TrackId TrackId, Title Title, Artist Artist, FileName FileName, AudioFormat AudioFormat);
